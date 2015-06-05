03fc27196bf23a9ca3a14afff6b0e4a607fc2122
Kento Masukawa <masukawa@west.sd.keio.ac.jp>
2015-06-05 22:43:14 +0900

    Initial Commit

---
+++ prepa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prepa
+//
+//  Created by 増川健人 on 2015/06/05.
+//  Copyright (c) 2015年 Kento. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
